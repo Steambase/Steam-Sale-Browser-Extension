a9a43c31c2473063959583996e1e3a02ece6d874
Lucas <lucas@coinledger.io>
2023-09-21 20:12:57 -0500

    Working New Tab

---
+++ src/tabs/delta-flyer.tsx
@@ -0,0 +1,18 @@
+import "../styles.css"
+
+function DeltaFlyerPage() {
+    return (
+        <div className="bg-slate-300"
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                padding: 16
+            }}>
+            <h2>Delta Flyer Tab</h2>
+
+            <p>This tab is only available on the Delta Flyer page.</p>
+        </div>
+    )
+}
+
+export default DeltaFlyerPage
